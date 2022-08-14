8e0d5b9d345539f5aa2090f3ec915cf9e03ccafd
Onlyjoy <kujaku@yandex.ru>
2022-08-14 10:04:06 +0300

    Lesson3_HW_done

--- bfe_framework/templator.py
+++ bfe_framework/templator.py
@@ -1,16 +1,14 @@
-from jinja2 import Template
-import os
+from jinja2 import Template, FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
     """
     :param template_name: имя шаблона
-    :param folder: папка в которой ищем шаблон
     :param kwargs: параметры
     :return:
     """
-    file_path = os.path.join(folder, template_name)
-
-    with open(file_path, encoding='utf-8') as f:
-        template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
