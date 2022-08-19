889da998885429d57222cd00b513e5ba41a7360e
Onlyjoy <kujaku@yandex.ru>
2022-08-19 12:53:20 +0300

    Lesson4_HW_done

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from datetime import date
-from views import Index, About
+from views import Index, About, StudyPrograms, CoursesList, CreateCourse, \
+    CreateCategory, CategoryList, CopyCourse
 
 
 # front controller
@@ -16,4 +17,10 @@ fronts = [secret_front, other_front]
 routes = {
     '/': Index(),
     '/about/': About(),
+    '/study-programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
+    '/copy-course/': CopyCourse()
 }
--- views.py
+++ views.py
@@ -1,9 +1,15 @@
+from datetime import date
+
 from bfe_framework.templator import render
+from patterns.creational_patterns import Engine, Logger
+
+site = Engine()
+logger = Logger('main')
 
 
 class Index:
     def __call__(self, request):
-        return '200 OK', render('index.html', data=request.get('data', None))
+        return '200 OK', render('index.html', objects_list=site.categories)
 
 
 class About:
@@ -11,6 +17,101 @@ class About:
         return '200 OK', render('about.html')
 
 
+class StudyPrograms:
+    def __call__(self, request):
+        return '200 OK', render('study-programs.html', data=date.today())
+
+
+class CoursesList:
+    def __call__(self, request):
+        logger.log('Список курсов')
+        try:
+            category = site.find_category_by_id(int(request['request_params']['id']))
+            return '200 OK', render('course_list.html', objects_list=category.courses,
+                                    name=category.name, id=category.id)
+        except KeyError:
+            return '200 OK', 'No courses have been added yet'
+
+
 class NotFound404:
     def __call__(self, request):
         return '404 WHAT', render('404.html')
+
+
+class CreateCourse:
+    category_id = -1
+
+    def __call__(self, request):
+        if request['method'] == 'POST':
+            data = request['data']
+
+            name = data['name']
+            name = site.decode_value(name)
+
+            category = None
+            if self.category_id != -1:
+                category = site.find_category_by_id(int(self.category_id))
+
+                course = site.create_course('record', name, category)
+                site.courses.append(course)
+
+            return '200 OK', render('course_list.html', objects_list=category.courses,
+                                    name=category.name, id=category.id)
+
+        else:
+            try:
+                self.category_id = int(request['request_params']['id'])
+                category = site.find_category_by_id(int(self.category_id))
+
+                return '200 OK', render('create_course.html', name=category.name, id=category.id)
+            except KeyError:
+                return '200 OK', 'No categories have been added yet'
+
+
+class CreateCategory:
+    def __call__(self, request):
+
+        if request['method'] == 'POST':
+            print(request)
+            data = request['data']
+
+            name = data['name']
+            name = site.decode_value(name)
+
+            category_id = data.get('category_id')
+
+            category = None
+            if category_id:
+                category = site.find_category_by_id(int(category_id))
+
+            new_category = site.create_category(name, category)
+            site.categories.append(new_category)
+
+            return '200 OK', render('index.html', objects_list=site.categories)
+        else:
+            categories = site.categories
+            return '200 OK', render('create_category.html', categories=categories)
+
+
+class CategoryList:
+    def __call__(self, request):
+        logger.log('Список категорий')
+        return '200 OK', render('category_list.html', objects_list=site.categories)
+
+
+class CopyCourse:
+    def __call__(self, request):
+        request_params = request['request_params']
+
+        try:
+            name = request_params['name']
+            old_course = site.get_course(name)
+            if old_course:
+                new_name = f'copy_{name}'
+                new_course = old_course.clone()
+                new_course.name = new_name
+                site.courses.append(new_course)
+
+            return '200 OK', render('course_list.html', objects_list=site.courses)
+        except KeyError:
+            return '200 OK', 'No courses have been added yet'
